a80ff39645caa1244a390efde8a22588c89a3cd1
Varun Bale <varun.bale@gmail.com>
2020-05-13 12:53:41 +0530

    Template driven forms part-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'; 
 import { FlexLayoutModule } from '@angular/flex-layout'; 
 
 import { AppComponent } from './app.component';
@@ -49,7 +53,11 @@ import { LoginComponent } from './login/login.component';
     MatCardModule,
     MatButtonModule,
     MatDialogModule,
-    FlexLayoutModule
+    MatFormFieldModule, 
+    MatInputModule,
+    MatCheckboxModule,
+    FlexLayoutModule,
+    FormsModule
   ],
   providers: [
     DishService,
